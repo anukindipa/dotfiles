c33dcd43cf91eae9b39f6a0441628542e2f55f8a
Anuk Indipa <61876990+anukindipa@users.noreply.github.com>
2022-02-15 11:49:21 +0530

    I am an idiot
    fixed the low battery script for polybar

--- useless-bash-scripts/batt.sh
+++ useless-bash-scripts/batt.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
-battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
 
 while true
 do
+battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
 if [ $battery_level -le 16 ]; then
 	notify-send --urgency=CRITICAL "Battery Low" "Level: ${battery_level}%"
       	paplay /usr/share/sounds/freedesktop/stereo/dialog-error.oga
