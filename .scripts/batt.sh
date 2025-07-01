1db0f3b7b4fd43101a196d81262896ac7f4a674f
Anuk Indipa <61876990+anukindipa@users.noreply.github.com>
2025-07-01 12:53:24 +1000

    changes

--- .scripts/batt.sh
+++ .scripts/batt.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
-is_discharging=`acpi -b | grep -o 'Discharging'`
+is_discharging=`acpi -b | grep -o 'Discharging'`1
 
-if [ $battery_level -le 18 ] && [ $is_discharging=='Discharging' ]; then
+if [ $battery_level -le 18 ] && [ $is_discharging=='Discharging1' ]; then
 	notify-send --urgency=CRITICAL "Battery Low" "Level: ${battery_level}%"
       	paplay /usr/share/sounds/freedesktop/stereo/dialog-error.oga
 fi
